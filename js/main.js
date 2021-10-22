162b995c358c655bc146906f5e8c90cdd41e8a64
ElWazy <ElCazero.Fer@gmail.com>
2021-10-22 08:49:50 -0300

    first screen "Tool Catalog" finished

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
-console.log("HI");
+window.addEventListener('DOMContentLoaded', (event) => {
+    const MIN_TOOLS = 30;
+    const MAX_TOOLS = 1000;
+
+    fetch("/js/persistence/tool_names.json")
+    .then(data => data.json())
+    .then(json => {
+        const grid = document.querySelector(".grid");
+        for (let i = 0; i < MIN_TOOLS; i++) {
+
+            const criticalStock = json[i].stock_actual > 10;
+            let color = "";
+
+            if (criticalStock) {
+                color = "primary";
+            } else {
+                color = "danger"
+            }
+
+            grid.insertAdjacentHTML(
+                'afterbegin',
+                `<div class="card flex-column">
+                    <p class="card-name ${color}">${json[i].name}</p>
+                    <div class="card-body ${color}">
+                        <img src="${json[i].url_image}"
+                                loading="lazy" 
+                                alt="imagen herramienta"/>
+                                <p class="card-category">${json[i].category}</p>
+                                <p>stock total: ${json[i].stock_total}</p>
+                        <p>stock real: ${json[i].stock_actual}</p>
+                    </div>
+                </div>`
+            );
+        }
+    });
+});
