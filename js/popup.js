10bb840d0a83dd95d7fedff29adc48bd9021d33e
Esteban Vivanco <esteban.vivanco.progra@gmail.com>
2021-11-03 13:21:04 -0300

    Intento de pop up sin framework
    como no podi usar frameworks loco ah ya ok

---
+++ js/popup.js
@@ -0,0 +1,12 @@
+const open = document.getElementById('open');
+const modalcontainer = document.getElementById('modalcontainer');
+const close = document.getElementById('close');
+
+
+open.addEventListener('click', ()=> {
+    modalcontainer.classList.add('show');
+})
+
+close.addEventListener('click', ()=> {
+    modalcontainer.classList.remove('show');
+})
