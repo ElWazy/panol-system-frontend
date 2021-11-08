98125575b747c02a086bd62eb44bbe1fcfe0c3da
Esteban Vivanco <esteban.vivanco.progra@gmail.com>
2021-11-08 17:52:04 -0300

    Modal de Create y Update Tool
    puede copiar y pegarlos en los otros modal ah listo alo

--- js/popup.js
+++ js/popup.js
@@ -2,6 +2,10 @@ const open = document.getElementById('open');
 const modalcontainer = document.getElementById('modalcontainer');
 const close = document.getElementById('close');
 
+const open2 = document.getElementById('open2');
+const modalcontainer2 = document.getElementById('modalcontainer2');
+const close2 = document.getElementById('close2');
+
 
 open.addEventListener('click', ()=> {
     modalcontainer.classList.add('show');
@@ -10,4 +14,14 @@ open.addEventListener('click', ()=> {
 close.addEventListener('click', ()=> {
     modalcontainer.classList.remove('show');
 })
+
+open2.addEventListener('click', ()=> {
+    modalcontainer2.classList.add('show-2');
+})
+
+close2.addEventListener('click', ()=> {
+    modalcontainer2.classList.remove('show-2');
+})
+
+
 
