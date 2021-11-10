861df2e720563cfbd3105aee1d945d72b9c2dc4b
Esteban Vivanco <esteban.vivanco.progra@gmail.com>
2021-11-09 22:25:54 -0300

    Arreglamiento de los popis alo
    alo

--- js/popup.js
+++ js/popup.js
@@ -1,27 +1,16 @@
-const open = document.getElementById('open');
-const modalcontainer = document.getElementById('modalcontainer');
-const close = document.getElementById('close');
 
-const open2 = document.getElementById('open2');
-const modalcontainer2 = document.getElementById('modalcontainer2');
-const close2 = document.getElementById('close2');
+function mostrar(){
+    document.getElementById('pup-1').style.display="grid";
+}
 
+function ocultar(){
+    document.getElementById('pup-1').style.display="block";
+}
 
-open.addEventListener('click', ()=> {
-    modalcontainer.classList.add('show');
-})
+function mostrar2(){
+    document.getElementById('pup-2').style.display="grid";
+}
 
-close.addEventListener('click', ()=> {
-    modalcontainer.classList.remove('show');
-})
-
-open2.addEventListener('click', ()=> {
-    modalcontainer2.classList.add('show-2');
-})
-
-close2.addEventListener('click', ()=> {
-    modalcontainer2.classList.remove('show-2');
-})
-
-
-
+function ocultar2(){
+    document.getElementById('pup-2').style.display="block";
+}
